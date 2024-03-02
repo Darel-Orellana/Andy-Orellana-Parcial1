66fa4c9c580e264c1a4e162b8c4cd78ad4a8ebc0
Darel-Orellana <160997288+Darel-Orellana@users.noreply.github.com>
2024-03-02 10:45:20 -0600

    Add files via upload
    no pude hacer lo de commit pero por entregarlo aca esta

---
+++ Proyectos(PROGRA)1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num = 0;
+    int p = 3;
+    while (num <31){
+
+        if (num + p);
+        {
+            cout << num + p << endl;
+
+            num++;
+
+        }
+    }
+
+//No me dio tiempo de elaborarlo bien pero aca va una parte
+    return 0;
+
+
+}
